97f98c37210762c6c0140cb85445dfa0fff92439
ziad amr shalaby <zezoamr@yandex.com>
2024-08-07 15:10:44 +0300

    feat finished pricerating component

---
+++ tripma/components/priceRating.js
@@ -0,0 +1,32 @@
+import styles from './priceRating.module.css'
+
+export default function PriceRating({ recommendation }) {
+    return (
+        <div className={styles.container}>
+            
+            <div className={styles.title}>
+                <div className={styles.Pricerating}>
+                Price rating
+                </div>
+
+                <div className={styles.basepricebadge}>
+                    <label>
+                    Buy soon
+                    </label>
+                </div>
+            </div>
+
+
+            <div className={styles.textContainer}>
+                <div className={styles.recommendation}>
+                    {recommendation}
+                </div>
+                <div className={styles.textstyle1}>
+                    Tripma analyzes thousands of flights, prices, and trends to ensure you get the best deal.
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
